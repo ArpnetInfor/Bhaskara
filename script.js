68fce0f9fd5394c7752d89672d7162ebb0312ced
Davi Faria dos Santos <tron_davi@hotmail.com>
2022-11-11 17:23:44 -0300

    restructuring

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ let calculator = document.getElementById('calcular')
 
 calculator.addEventListener('click', ()=>{
     let a = document.getElementById('num1').value
-    let b = document.getElementById('num2').value
-    let c = document.getElementById('num3').value
-    let condicao = document.getElementById('condicao')
-    let resultado1 = document.getElementById('res1')
-    let resultado2 = document.getElementById('res2')
+        let b = document.getElementById('num2').value
+            let c = document.getElementById('num3').value
+                let condicao = document.getElementById('condicao')
+                    let resultado1 = document.getElementById('res1')
+                        let resultado2 = document.getElementById('res2')
     let delta
     let x1
     let x2
@@ -16,18 +16,34 @@ calculator.addEventListener('click', ()=>{
     x1 = (-b + Math.sqrt(delta))/ 2*a
     x2 = (-b - Math.sqrt(delta))/ 2*a
 
-    if (delta > 0) {
+
+    if (delta == "") {
+        alert("Digitar os valores de a, b, c")
+        document.getElementById('num1').focus()
+    }else if (delta > 0) {
         condicao.innerHTML = (`Delta: ${delta} - Raízes reais e distintas.`)
-        resultado1.innerHTML = x1.toFixed(2)
-        resultado2.innerHTML = x2.toFixed(2) 
+            resultado1.innerHTML = x1.toFixed(2)
+                resultado2.innerHTML = x2.toFixed(2)
+                    document.getElementById('num1').value = "" 
+                        document.getElementById('num2').value = "" 
+                            document.getElementById('num3').value = ""
+                                document.getElementById('num1').focus()
     }else if(delta == 0){
         condicao.innerHTML = (`Delta: ${delta} - Somente uma raiz real.`)
-        resultado1.innerHTML = x1.toFixed(2)
-        resultado2.innerHTML = x2.toFixed(2)  
+            resultado1.innerHTML = x1.toFixed(2)
+                resultado2.innerHTML = x2.toFixed(2) 
+                    document.getElementById('num1').value = "" 
+                        document.getElementById('num2').value = "" 
+                            document.getElementById('num3').value = ""
+                                document.getElementById('num1').focus() 
     }else if(delta < 0){
         condicao.innerHTML = (`Delta: ${delta} - Não possui raízes reais.`)
-        resultado1.innerHTML = "0"
-        resultado2.innerHTML = "0"
+            resultado1.innerHTML = "0"
+                resultado2.innerHTML = "0"
+                    document.getElementById('num1').value = "" 
+                        document.getElementById('num2').value = "" 
+                            document.getElementById('num3').value = ""
+                                document.getElementById('num1').focus()
     }
 
     
